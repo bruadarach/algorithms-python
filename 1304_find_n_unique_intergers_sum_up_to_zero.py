41b2983522113168040e55f28d5d8738c707b8d5
Sujeong Ji <suji.sujeongji@gmail.com>
2020-12-17 17:16:41 +0900

    1304_find_n_unique_intergers_sum_up_to_zero.py

---
+++ 1304_find_n_unique_intergers_sum_up_to_zero.py
@@ -0,0 +1,72 @@
+'''
+Given an integer n, return any array containing n unique integers such that they add up to 0.
+
+
+Example 1:
+
+Input: n = 5
+Output: [-7,-1,1,3,4]
+Explanation: These arrays also are accepted [-5,-1,1,2,3] , [-3,-1,2,-2,4].
+
+Example 2:
+
+Input: n = 3
+Output: [-1,0,1]
+
+Example 3:
+
+Input: n = 1
+Output: [0]
+ 
+
+Constraints:
+
+1 <= n <= 1000
+'''
+
+
+class Solution:
+    def sumZero(self, n: int) -> List[int]:
+
+        result = []
+
+        if n % 2 == 1:
+            for i in range((n//2)+1):
+                if i != 0:
+                    result.append(i)
+                    result.append(-i)
+                else:
+                    result.append(i)
+
+        else:
+            for i in range(n-1):
+                result.append(i)
+            if sum(result) > 0:
+                result.append(-sum(result))
+            elif sum(result) < 0:
+                result.append(sum(result))
+        return result
+
+# (runtime / memory)
+#  28 ms / 14.5 MB
+
+
+
+'''
+class Solution:
+    def sumZero(self, n: int) -> List[int]:
+       
+        if n % 2 :
+            result = [0]
+            n -= 1
+        else:
+            result=[]
+            
+        for i in range(1,(n//2)+1):
+            result.append(i)
+            result.append(-i)
+            
+        return result
+'''
+# (runtime / memory)
+#  36 ms / 14.4 MB
