eaa5b4ed475832e509e48c41cbf5a3ed9c4f84fa
Sujeong Ji <suji.sujeongji@gmail.com>
2021-01-28 23:55:05 +0900

    priority_queue.py

--- data_structures_algorithms/priority_queue.py
+++ data_structures_algorithms/priority_queue.py
@@ -1,6 +1,7 @@
 ''' implementing Priority Queue in Python'''
 
 ### 1. using 'List' ###
+import heapq
 from queue import PriorityQueue
 
 q = []
@@ -27,3 +28,28 @@ while q:
 # (3, 'Jake')
 # (2, 'Minji')
 # (1, 'Suji')
+
+
+
+### 2. using 'headpq' ###
+
+q = []
+
+heapq.heappush(q, (2, "Minji"))
+heapq.heappush(q, (3, "Jake"))
+heapq.heappush(q, (1, "Suji"))
+heapq.heappush(q, (4, "Lucky"))
+print(q)  # [(1, 'Suji'), (3, 'Jake'), (2, 'Minji'), (4, 'Lucky')]
+
+while q:
+    print(q[0]) 
+    print(heapq.heappop(q))
+'''
+while q:
+    next_item = heapq.heappop(q)
+    print(next_item)
+'''
+# (1, 'Suji')
+# (2, 'Minji')
+# (3, 'Jake')
+# (4, 'Lucky')
