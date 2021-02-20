40926e767105b4321ffdd2525bd95129239b6101
Sujeong Ji <suji.sujeongji@gmail.com>
2021-02-20 11:31:25 +0900

    1200_minimum_absolute_difference.py

---
+++ 1200_minimum_absolute_difference.py
@@ -0,0 +1,57 @@
+'''
+Given an array of distinct integers arr, find all pairs of elements with the minimum absolute difference of any two elements. 
+Return a list of pairs in ascending order(with respect to pairs), each pair [a, b] follows
+
+a, b are from arr
+a < b
+b - a equals to the minimum absolute difference of any two elements in arr
+ 
+
+Example 1:
+
+Input: arr = [4,2,1,3]
+Output: [[1,2],[2,3],[3,4]]
+Explanation: The minimum absolute difference is 1. List all pairs with difference equal to 1 in ascending order.
+
+Example 2:
+
+Input: arr = [1,3,6,10,15]
+Output: [[1,3]]
+
+Example 3:
+
+Input: arr = [3,8,-10,23,19,-4,-14,27]
+Output: [[-14,-10],[19,23],[23,27]]
+ 
+
+Constraints:
+
+2 <= arr.length <= 10^5
+-10^6 <= arr[i] <= 10^6
+'''
+
+
+class Solution:
+    def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+
+        if len(arr) < 2:
+            return []
+
+        arr = sorted(arr)
+        temp = []
+        diff = abs(arr[1]-arr[0])
+
+        for i in range(len(arr)-1):
+            if abs(arr[i+1] - arr[i]) <= diff:
+                temp.append([arr[i], arr[i+1]])
+                diff = abs(arr[i+1] - arr[i])
+
+        result = []
+        for j in range(len(temp)):
+            if abs(temp[j][1] - temp[j][0]) == diff:
+                result.append(temp[j])
+
+        return result
+
+# (runtime / memory)
+#  308 ms / 28.2 MB
