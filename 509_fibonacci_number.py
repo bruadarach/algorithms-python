2e121d288111dc72145d2a966db4aa8b985a17df
Sujeong Ji <suji.sujeongji@gmail.com>
2021-03-11 02:22:07 +0900

    509_fibonacci_number.py

--- 509_fibonacci_number.py
+++ 509_fibonacci_number.py
@@ -109,3 +109,15 @@ class Solution:
 # (runtime / memory)
 #  28 ms / 14.2 M
 
+
+
+''' ##### recursion #####
+class Solution:
+    def fib(self, n: int) -> int:
+        
+        if n == 0 or n == 1:
+            return n
+        return self.fib(n - 1) + self.fib(n - 2)
+'''
+# (runtime / memory)
+#  916 ms / 14.3 M
