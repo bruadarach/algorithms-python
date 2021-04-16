811fc3f592a2c32e637eb5d1f423b2d0a74735b0
Sujeong Ji <suji.sujeongji@gmail.com>
2021-04-17 00:10:12 +0900

    21_merge_two_sorted_lists.py

--- 21_merge_two_sorted_lists.py
+++ 21_merge_two_sorted_lists.py
@@ -27,5 +27,41 @@ The number of nodes in both lists is in the range [0, 50].
 Both l1 and l2 are sorted in non-decreasing order.
 '''
 
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+
+        if l1 is None:
+            return l2
+        if l2 is None:
+            return l1
+        if l1 is None and l2 is None:
+            return None
+
+        head = ListNode(0)  # ListNode{val: 0, next: None}
+        current = head
+
+        while l1 and l2:
+
+            if l1.val > l2.val:
+                current.next = l2
+                l2 = l2.next
+
+            else:  # l1.val <= l2.val
+                current.next = l1
+                l1 = l1.next
+
+            current = current.next
+
+        current.next = l1 or l2
+
+        return head.next
+
 # (runtime / memory)
-#  44 ms / 15 MB
+#  28 ms / 13.9 MB
