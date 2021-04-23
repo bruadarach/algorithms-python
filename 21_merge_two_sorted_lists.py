0ca4362f47f2d4f6a8dd5b4c0f0c058dffb3cd0b
Sujeong Ji <suji.sujeongji@gmail.com>
2021-04-24 00:24:34 +0900

    21_merge_two_sorted_lists.py

--- 21_merge_two_sorted_lists.py
+++ 21_merge_two_sorted_lists.py
@@ -28,6 +28,8 @@ Both l1 and l2 are sorted in non-decreasing order.
 '''
 
 
+##### Iteration #####
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -65,3 +67,32 @@ class Solution:
 
 # (runtime / memory)
 #  28 ms / 13.9 MB
+
+
+
+##### Recursion ##### 
+'''
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        
+        if not l1: 
+            return l2
+        if not l2: 
+            return l1
+        if not l1 and not l2: 
+            return None 
+        
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else: # l1.val < l2.val:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
+'''
+# (runtime / memory)
+#  32 ms / 14.2 MB
