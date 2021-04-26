0b7579f6f3b5ca471b3f658ee8c78961c350b4c7
Sujeong Ji <suji.sujeongji@gmail.com>
2021-04-26 18:53:02 +0900

    1339_count_largest_group.py

--- 1339_count_largest_group.py
+++ 1339_count_largest_group.py
@@ -30,3 +30,55 @@ Constraints:
 
 1 <= n <= 10^4
 '''
+
+
+##### Using defaultdict #####
+class Solution:
+    def countLargestGroup(self, n: int) -> int:
+        
+        if n < 10:
+            return n
+       
+        l = collections.defaultdict(int)
+        for i in range(1,n+1):
+            sum = 0
+            for j in str(i):
+                sum += int(j)
+            l[sum] += 1
+            sum = 0
+
+        largest_size = max(l.values())
+        count = 0
+        for n, c in l.items():
+            if c == largest_size:
+                count += 1
+        return count
+
+# (runtime / memory)
+#  104 ms / 14.2 MB
+
+
+
+##### Using dictionary #####
+'''
+class Solution:
+    def countLargestGroup(self, n: int) -> int:
+
+        if n < 10:
+            return n
+       
+        d = {}
+        
+        for i in range(1, n+1):
+            num_sum = sum(map(int, list(str(i))))
+            
+            if num_sum not in d:
+                d[num_sum] = 1
+            else:
+                d[num_sum] += 1
+        
+        largest_size = max(d.values())
+        return sum(1 for i in d.values() if i == largest_size)
+'''
+# (runtime / memory)
+#  116 ms / 14.3 MB
